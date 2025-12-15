564e19fa99614041923ccfc357a98ba593324c4a
Jonathan Gryphon <jonathangryphon@Jonathans-MacBook-Pro.local>
2025-12-15 21:05:09 +0100

    updated zfs.nix to include mount points for datasets for ente, headscale, traefik, the pool's root, and a comment for nextcloud or some other general file storage solution to replace iCloud

--- nix-configs/zfs.nix
+++ nix-configs/zfs.nix
@@ -4,18 +4,42 @@ let
   pool = "tank";
 in
 {
-  fileSystems."/tank/photos" = {
-    device = "${pool}/photos";
+  # Mount pool root for simplified observability
+  fileSystems."/tank" = {
+    device = "tank";
     fsType = "zfs";
   };
-  fileSystems."/tank/databases" = {
-    device = "${pool}/databases";
+
+  # Mount Ente storage
+  fileSystems."/var/lib/ente" = {
+    device = "${pool}/ente";
     fsType = "zfs";
   };
+
+  # Mount Headscale storage
+  fileSystems."/var/lib/headscale" = {
+    device = "${pool}/headscales";
+    fsType = "zfs";
+  };
+
+  # Mount Traefik storage
+  fileSystems."/var/lib/traefik" = {
+    device = "${pool}/traefik";
+    fsType = "zfs";
+  };
+
+  # Mount Nextcloud or other self hosted general file storage solution
 
   # Optional: ZFS dataset properties
   environment.etc."zfs-properties".text = ''
     zfs set compression=lz4 ${pool}
     zfs set atime=off ${pool}/photos
   '';
+
+  services.zfs = {
+    autoScrub.enable = true;
+    trim.enable = true;
+  };
+
+  networking.hostId = "4n2c8i5e"; #unique, required for zfs to avoid corruption somehow?
 }
