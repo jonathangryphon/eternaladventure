616a7383b12b478ddad9e49ef45f733f2cfbecac
Jonathan Gryphon <jonathangryphon@Jonathans-MacBook-Pro.local>
2025-12-15 13:45:58 +0100

    after encrypting the private keys and researching ssh details more, I copied the proper public keys to the ssh.nix module for users charity and breakglass

--- nix-configs/ssh.nix
+++ nix-configs/ssh.nix
@@ -1,10 +1,24 @@
-services.openssh = {
-    enable = true;
-    ports = [ 65555 ];
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
-      AllowUsers = [ "charity" ];
+{ config, pkgs, ... }:
+
+{
+  services.openssh = {
+      enable = true;
+      settings = {
+        Port  = [ 65555 ]; #security through obscurity to avoid default port scanning of 22
+        PasswordAuthentication = false;
+        PubkeyAuthentication = true; # unnecessary I think, but declares intent
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = [ "charity" ];
+      };
     };
-  };
+
+  # Associate SSH keys with users
+  users.users.charity.openssh.authorizedKeys.keys = [
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHawf4YO7tfG/BkWfw0E+aQRThKTIsGjXSwDBfQK/VGF charity@macbook
+  ];
+
+  users.users.breakglass.openssh.authorizedKeys.keys = [
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGPtcNoxFq+KnnCvt5xmBAOfzLXWul3i0MmOA8W/FXl breakglass@macbook
+  ];  
+}
