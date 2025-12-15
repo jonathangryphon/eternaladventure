c15d6886a592c5c1623890d2aff21784707c7475
Jonathan Gryphon <jonathangryphon@Jonathans-MacBook-Pro.local>
2025-12-15 23:33:02 +0100

    updated traefik_dynamic.nix to have an example and a headscale bit. both should be valid

--- nix-configs/services/traefik/traefik_dynamic.nix
+++ nix-configs/services/traefik/traefik_dynamic.nix
@@ -8,11 +8,12 @@ in
     "d ${dynDir} 0750 traefik traefik -"
   ];
 
+  # Generate Test YAML
   environment.etc."traefik/dynamic/test.yaml".text = ''
     http:
       routers:
         test:
-          rule: "Host(`test.afabel.net`)"
+          rule: "Host(`test.eternaladventure.xyz`)"
           entryPoints: [ websecure ]
           service: test
           tls:
@@ -23,6 +24,23 @@ in
           loadBalancer:
             servers:
               - url: "http://127.0.0.1:65535"
+  '';
+  # Generate Headscale YAML
+  environment.etc."traefik/dynamic/headscale.yml".text = ''
+    http:
+      routers:
+        headscale:
+          rule: "Host(`headscale.${cfg.domain}`)"
+          entryPoints:
+            - websecure
+          service: headscale
+          tls:
+            certResolver: myresolver
+      services:
+        headscale:
+          loadBalancer:
+            servers:
+              - url: "http://localhost:${cfg.headscalePort}"
   '';
 }
 
