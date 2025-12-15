054937635c44ecebd19e129435fb054e3063304f
Jonathan Gryphon <jonathangryphon@Jonathans-MacBook-Pro.local>
2025-12-15 22:19:21 +0100

    created minimal traefik skele with chatgpt help

--- nix-configs/services/traefik.nix
+++ nix-configs/services/traefik.nix
@@ -1,5 +1,46 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  dataDir = "/var/lib/traefik";
+in
 {
-  services.traefik.enable = true;
+  services.traefik = {
+    enable = true;
+
+    staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = ":80";
+          http.redirections.entryPoint = {
+            to = "websecure";
+            scheme = "https";
+          };
+        };
+
+        websecure = {
+          address = ":443";
+        };
+      };
+
+      providers.file = {
+        directory = "${dataDir}/dynamic";
+        watch = true;
+      };
+
+      certificatesResolvers.letsencrypt.acme = {
+        email = "eternaladventure@proton.me";
+        storage = "${dataDir}/acme.json";
+        httpChallenge.entryPoint = "web";
+      };
+
+      log.level = "INFO";
+    };
+  };
+
+  systemd.services.traefik.serviceConfig = {
+    StateDirectory = "traefik";
+  };
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
+
