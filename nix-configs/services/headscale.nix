19015867ecfb20b23c0512dabb4c07329b3bfd1b
Jonathan Gryphon <jonathangryphon@Jonathans-MacBook-Pro.local>
2025-12-16 15:30:38 +0100

    updated headscale.nix to represent findings about dynamic configs with traefik and how it should be managed in each container's config file using dynamicConfigOptions module

--- nix-configs/services/headscale.nix
+++ nix-configs/services/headscale.nix
@@ -18,10 +18,25 @@
 
       log.level = "info";
     };
-  };
+    
+    loadBalancer.server = [
+      { url = "http://127.0.0.1:8080"; }
+    ]; 
+ };
 
   systemd.services.headscale.serviceConfig = {
     StateDirectory = "headscale";
+  };
+
+  # Traefik integration for networking/routing
+  services.traefik.dynamicConfigOptions.http = {
+    routers.headscale = {
+      rule = "Host(`headscale.eternaladventure.xyz`)";
+      entryPoints = [ "websecure" ];
+      service = "headscale";
+      tls.certResolver = "letsencrypt";
+      # TODO: Headscale needs HTTP/2 disabled behind proxies???
+      # middlewares = [ "headscale-headers" ];
   };
 }
 
