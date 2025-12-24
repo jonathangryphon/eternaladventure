f1a46ba8a4a567c7ef0c4cf8f4352a4089446f58
Jonathan Gryphon <jonathangryphon@macbookpro.lan>
2025-12-23 20:12:34 -0600

    updated sops-nix details in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -34,7 +34,7 @@ in
     ./modules/wifi.nix
 
     # Obtain sops-nix via fetchTarball
-    sopsNix/modules/sops
+    "${sopsNix}/modules/sops"
     ];
 
   ############################
@@ -48,7 +48,7 @@ in
   ############################
 #  networking.networkmanager.enable = true;
 # chatgpt didn't like for a server, said unnecessary, instead do below
-  networking.useDHCP = true;
+#  networking.useDHCP = true; # so I guess networkmanager takes over and this conflicts with it. see wifi.nix
   networking.firewall.allowedTCPPorts = [ 80 443 ]; # ssh port is defined in /modules/ssh.nix
 
   ############################
@@ -89,6 +89,11 @@ in
     sops
     age
   ];
+
+  ############################
+  # SOPS Key File Location
+  ############################
+  sops.age.keyFile = "/etc/nixos/secrets/keys.txt";
 
   ############################
   # First NixOS version installed
