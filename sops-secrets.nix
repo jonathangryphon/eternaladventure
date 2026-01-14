7c2088bacf176a4ac906cceb148f4a48d16249b8
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-01-13 20:25:29 -0700

    updating sops-secrets with new wifi addition:

--- sops-secrets.nix
+++ sops-secrets.nix
@@ -17,4 +17,8 @@
     sopsFile = ./secrets/wifi_secrets.yaml;
     # key = "wifi.sarah-wifi.psk"; # this key entry on each thing was erroring out because, essentially, sops was trying to eval the key twice. Once with the top line [sops.secrets."" =] and once with the key = bit. so it's entirely unnecessary
   };
+  
+  sops.secrets."wifi/koshka-wifi/psk" = {
+    sopsFile = ./secrets/wifi_secrets.yaml;
+  };
 }
