f2db7368d471ebae0af7eb0d8bdc1a21bdff67a3
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-01-16 17:31:13 -0700

    removed all fileSystem entries in zfs.nix

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -1,45 +1,8 @@
 { config, pkgs, ... }:
-
-let
-  pool = "tank";
-in
 {
-
-  # Mount pool root for simplified observability
-  fileSystems."/tank" = {
-    device = "tank";
-    fsType = "zfs";
-  };
-
-  # Mount Ente storage
-  fileSystems."/var/lib/ente" = {
-    device = "${pool}/ente";
-    fsType = "zfs";
-  };
-
-  # Mount Headscale storage
-  fileSystems."/var/lib/headscale" = {
-    device = "${pool}/headscales";
-    fsType = "zfs";
-  };
-
-  # Mount Traefik storage
-  fileSystems."/var/lib/traefik" = {
-    device = "${pool}/traefik";
-    fsType = "zfs";
-  };
-
-  # Mount Nextcloud or other self hosted general file storage solution
-
-  # Optional: ZFS dataset properties
-  #environment.etc."zfs-properties".text = ''
-   # zfs set compression=lz4 ${pool}
-    #zfs set atime=off ${pool}/photos
-  #'';
 
   services.zfs = {
     autoScrub.enable = true;
     trim.enable = true;
-#    autoImport.enable = true # this does't seem real according to search.nixos.org
   };
 }
