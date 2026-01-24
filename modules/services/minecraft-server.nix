37d9b3f0b1da43b84c36730979ab54427fc7ff9c
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-01-24 11:27:22 -0700

    updating mc server

--- modules/services/minecraft-server.nix
+++ modules/services/minecraft-server.nix
@@ -12,21 +12,21 @@
     openFirewall = true;
 
     package = pkgs.papermcServers.papermc-1_21_9;
-    dataDir = "/tank/services/minecraft";
+    dataDir = "/tank/services/minecraft-server";
 
     serverProperties = {
         minecraft-server.port = 25655;
     }
   };
   
-  services.traefik.dynamicConfigOptions.http.routers.nextcloud = {
+  services.traefik.dynamicConfigOptions.http.routers.minecraft-server = {
     rule = "Host(`minecraft.eternaladventure.xyz`)";
     entryPoints = [ "websecure" ];
-    service = "minecraft-service";
+    service = "minecraft-server";
     tls.certResolver = "letsencrypt";
   };
 
-  services.traefik.dynamicConfigOptions.http.services.minecraft-service = {
+  services.traefik.dynamicConfigOptions.http.services.minecraft-server = {
     loadBalancer.servers = [
       { url = "http://127.0.0.1:25655"; }
     ];
