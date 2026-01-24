a8a23a58749b8d6b6b8f77dfd081899b34837ce7
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-01-24 13:35:38 -0700

    updated mc server port name (removed minecraft subname)

--- modules/services/minecraft-server.nix
+++ modules/services/minecraft-server.nix
@@ -15,20 +15,7 @@
     dataDir = "/tank/services/minecraft-server";
 
     serverProperties = {
-        minecraft-server.port = 25655;
+        server-port = 33333;
     }
-  };
-  
-  services.traefik.dynamicConfigOptions.http.routers.minecraft-server = {
-    rule = "Host(`minecraft.eternaladventure.xyz`)";
-    entryPoints = [ "websecure" ];
-    service = "minecraft-server";
-    tls.certResolver = "letsencrypt";
-  };
-
-  services.traefik.dynamicConfigOptions.http.services.minecraft-server = {
-    loadBalancer.servers = [
-      { url = "http://127.0.0.1:25655"; }
-    ];
   };
 }
