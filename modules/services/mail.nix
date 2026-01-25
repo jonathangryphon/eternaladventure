4dfa2a410c6ef86f7971cb3d9ce3e63d206cdd88
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-01-24 17:22:09 -0700

    added mail server, in progress

---
+++ modules/services/mail.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+    services.maddy = {
+        enable = true;
+        openFirewall = true;
+        hostname = "mail.eternaladventure.xyz"
+        primaryDomain = "eternaladventure.xyz"
+
+        tls = {
+            enable = true;
+            mode = "acme";
+            acmeEmail = "admin@eternaladventure.xyz"
+        };
+
+        storage = {
+            type = "maildir";
+            path = "/tank/services/mail";
+        };
+
+        auth = {
+            type = "passwd";
+        }
+
+        
+    }
+}
