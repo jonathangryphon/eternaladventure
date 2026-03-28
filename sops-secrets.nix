f0ac2601a9cbd7f4580a3bc45ca912077787eb5d
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-03-28 10:46:56 -0500

    updated wifi.nix according to findings on backup server. this was never working in the first place

--- sops-secrets.nix
+++ sops-secrets.nix
@@ -9,17 +9,19 @@
     sopsFile = ./secrets/porkbun_secrets.yaml;
   };
 
-  sops.secrets."wifi/home-wifi/psk" = { 
-    sopsFile = ./secrets/wifi_secrets.yaml;
+  sops.secrets.home-wifi = {
+    sopsFile = ./secrets/home-wifi.env.enc;
+    format = "dotenv";
   };
-  
-  sops.secrets."wifi/sarah-wifi/psk" = { 
-    sopsFile = ./secrets/wifi_secrets.yaml;
-    # key = "wifi.sarah-wifi.psk"; # this key entry on each thing was erroring out because, essentially, sops was trying to eval the key twice. Once with the top line [sops.secrets."" =] and once with the key = bit. so it's entirely unnecessary
+
+  sops.secrets.nano-wifi = {
+    sopsFile = ./secrets/nano-wifi.env.enc;
+    format = "dotenv";
   };
-  
-  sops.secrets."wifi/koshka-wifi/psk" = {
-    sopsFile = ./secrets/wifi_secrets.yaml;
+
+  sops.secrets.koshka-wifi = {
+    sopsFile = ./secrets/koshka-wifi.env.enc;
+    format = "dotenv";
   };
   
   sops.secrets."nextcloud/admin_password" = {
