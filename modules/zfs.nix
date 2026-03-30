0aa84695c4bff85e3cb7a5a60e1a644ebe38c997
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-03-29 21:04:45 -0500

    updating server for syncoid

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -4,6 +4,25 @@
   services.zfs = {
     autoScrub.enable = true;
     trim.enable = true;
-    autoSnapshot.enable = true;
+    # autoSnapshot.enable = true; # replaced by sanoid below
+  };
+
+  services.syncoid = { # creates the syncoid user which the pi can use to remote in for backup pulls
+    enable = true;
+  };
+
+  services.sanoid = {
+    enable = true;
+    datasets = {
+      "tank/services" = {
+        recursive = true; # this covers all datasets beneath services
+        hourly = 24;
+        daily = 7;
+        weekly = 4;
+        monthly = 3;
+        autosnap = true;
+        autoprune = true;
+      };
+    };
   };
 }
