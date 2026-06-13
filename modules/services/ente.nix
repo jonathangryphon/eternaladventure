8d0146b7ceb2fd565513c80e24f3c90e8c0bab5f
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-06-13 09:38:54 +0200

    fixing museum port

--- modules/services/ente.nix
+++ modules/services/ente.nix
@@ -75,8 +75,6 @@
 
     settings = {
   
-      http-port = 2112;
-
       s3 = {
         are_local_buckets   = true;
         use_path_style_urls = true;
@@ -187,7 +185,7 @@
     };
 
     services = {
-      ente-api-service.loadBalancer.servers      = [{ url = "http://127.0.0.1:2112"; }];
+      ente-api-service.loadBalancer.servers      = [{ url = "http://127.0.0.1:8080"; }];
       ente-photos-service.loadBalancer.servers   = [{ url = "http://127.0.0.1:8083"; }];
       ente-accounts-service.loadBalancer.servers = [{ url = "http://127.0.0.1:8081"; }];
       ente-cast-service.loadBalancer.servers     = [{ url = "http://127.0.0.1:8082"; }];
