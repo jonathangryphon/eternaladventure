be5bc6e2670a13b784ef3967e92630c47b98cdca
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-06-17 13:55:45 +0200

    pinning sops-nix to the known working commit

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.url = "github:Mic92/sops-nix/9836912e37aef546029e48c8749834735a6b9dad";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
