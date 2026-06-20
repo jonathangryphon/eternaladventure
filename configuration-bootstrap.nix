6f121e3d64c05966de1fb4fa2ee175fa73ccdaff
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-06-20 22:52:02 +0200

    testing bootstrap

--- configuration-bootstrap.nix
+++ configuration-bootstrap.nix
@@ -1,9 +1,62 @@
-{ lib,  sops-nix, ... }:
+{ lib, pkgs, sops-nix, ... }:
 
 {
+  ############################
+  # Boot
+  ############################
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = [ "zfs" ];
+  boot.supportedFilesystems = [ "zfs" ];
+
+  ############################
+  # Networking & Firewall
+  ############################
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  ############################
+  # Host & Timezone
+  ############################
+  time.timeZone = "America/Chicago";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  ############################
+  # Security
+  ############################
+  security.sudo.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+
+  ############################
+  # Flake support
+  ############################
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  ############################
+  # System Packages
+  ############################
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    wget
+    htop
+    sops
+    age
+  ];
+
+  ############################
+  # First NixOS version installed
+  ############################
+  system.stateVersion = "25.11";
+
+  ############################
+  # Imports
+  ############################
   imports = [
     ./modules/server_arch.nix
+    ./modules/podman.nix
     ./modules/ssh.nix
+    ./modules/traefik.nix
     "${sops-nix}/modules/sops"
     ./sops-secrets.nix
     ./modules/sops.nix
