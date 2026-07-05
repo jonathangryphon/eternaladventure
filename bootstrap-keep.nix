27956301f7d8273d09141443b9c4d2e54d7d2fea
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-05 12:29:18 -0400

    removing ssh.nix from keep

--- bootstrap-keep.nix
+++ bootstrap-keep.nix
@@ -34,8 +34,31 @@
     extraGroups = [ "wheel" ]; # sudo access
   };
 
+  services.openssh = {
+      enable = true;
+      ports = [ 62022 ]; # security through obscurity to avoid default port 22 scanning via bots
+
+      settings = {
+        PasswordAuthentication = false;
+        PubkeyAuthentication = true; # this is the default, but declaring it expresses code intent
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        AllowUsers = [ "charity" "breakglass" "syncoid" ];
+      };
+    };
+  
+  networking.firewall.allowedTCPPorts = [ 62022 ]; # open port for ssh
+  
+  # Associate SSH keys with users
+  users.users.charity.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHawf4YO7tfG/BkWfw0E+aQRThKTIsGjXSwDBfQK/VGF charity@macbook"
+  ];
+
+  users.users.breakglass.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGPtcNoxFq+KnnCvt5xmBAOfzLXWul3i0MmOA8W/FXl breakglass@macbook"
+  ];  
+
   imports = [
-    ./modules/ssh.nix
     ./modules/traefik.nix
   ];
 
