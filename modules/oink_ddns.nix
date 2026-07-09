17767f785b720ffb404227d2076c78f078c5078e
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-09 11:54:10 -0400

    disabling oink_ddns now that vps forward is set up

--- modules/oink_ddns.nix
+++ modules/oink_ddns.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 {
   services.oink = {
-    enable = true;
+    enable = false;
     apiKeyFile = "/run/secrets/porkbun/apikey";
     secretApiKeyFile = "/run/secrets/porkbun/secretapikey";
     settings.interval = 900; # seconds between updates
