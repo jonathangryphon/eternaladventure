e5085ad50965b5b06740d400acabe4c7ad672f04
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-10 13:03:49 -0400

    trying out headscale. fixing bugs

--- modules/services/headscale-server.nix
+++ modules/services/headscale-server.nix
@@ -9,4 +9,17 @@
             dns.nameservers.global = [ "9.9.9.9" "149.112.112.112" ];
         };
     };
+    # Traefik Required Bits
+    services.traefik.dynamicConfigOptions.http.routers.headscale = {
+        rule = "Host(`headscale.eternaladventure.xyz`)";
+        entryPoints = [ "websecure" ];
+        service = "headscale-service";
+        tls.certResolver = "letsencrypt";
+    };
+
+    services.traefik.dynamicConfigOptions.http.services.headscale-service = {
+        loadBalancer.servers = [
+            { url = "http://127.0.0.1:8080"; }
+        ];
+    };
 }
