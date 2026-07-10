551bb86bf70cf364e1aa6af340584b1dbcccb9af
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-10 12:51:28 -0400

    trying out headscale

--- modules/services/headscale-server.nix
+++ modules/services/headscale-server.nix
@@ -6,6 +6,7 @@
             server_url = "https://headscale.eternaladventure.xyz";
             dns.magic_dns = true;
             dns.base_domain = "ts.eternaladventure.xyz";
+            dns.nameservers.global = [ "9.9.9.9" "149.112.112.112" ];
         };
     };
 }
