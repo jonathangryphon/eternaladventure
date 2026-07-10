f4f441caacb629a5a657de67c4735bf1fbdcd04a
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-10 13:32:48 -0400

    adding tcp forward for hs

--- modules/traefik_passthrough.nix
+++ modules/traefik_passthrough.nix
@@ -7,7 +7,7 @@
       entryPoints.web.address = ":80";
       entryPoints.websecure.address = ":443";
       entryPoints.mc.address = ":33333";
-      entryPoints.headscale.address = ":4090";
+      # entryPoints.headscale.address = ":4090";
     };
     dynamicConfigOptions.tcp = {
       routers = {
@@ -27,17 +27,17 @@
           rule = "HostSNI(`*`)";
           service = "mc-home";
         };
-        headscale-passthrough = {
-          entryPoints = [ "headscale" ];
-          rule = "HostSNI(`*`)";
-          service = "headscale-home";
-        };
+        #headscale-passthrough = {
+         # entryPoints = [ "headscale" ];
+          #rule = "HostSNI(`*`)";
+          #service = "headscale-home";
+        #};
       };
       services = {
         home-https.loadBalancer.servers = [{ address = "10.100.0.2:443"; }];
         home-http.loadBalancer.servers  = [{ address = "10.100.0.2:80"; }];
         mc-home.loadBalancer.servers    = [{ address = "10.100.0.2:33333"; }];
-        headscale-home.loadBalancer.servers = [{ address = "10.100.0.2:4090"; }];
+        # headscale-home.loadBalancer.servers = [{ address = "10.100.0.2:4090"; }];
       };
     };
   };
