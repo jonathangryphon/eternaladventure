1cd816bea5727aeb00abe4768eb546d1bfff3ff4
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-10 17:48:09 -0400

    cleaning traefik-passthrough file

--- modules/traefik_passthrough.nix
+++ modules/traefik_passthrough.nix
@@ -7,7 +7,6 @@
       entryPoints.web.address = ":80";
       entryPoints.websecure.address = ":443";
       entryPoints.mc.address = ":33333";
-      # entryPoints.headscale.address = ":4090";
     };
     dynamicConfigOptions.http = {
       routers.http-home = {
@@ -24,27 +23,16 @@
           service = "home-https";
           tls.passthrough = true;
         };
-        #http-passthrough = {
-         # entryPoints = [ "web" ];
-          #rule = "HostSNI(`*`)";
-          #service = "home-http";
-        #};
         mc-passthrough = {
           entryPoints = [ "mc" ];
           rule = "HostSNI(`*`)";
           service = "mc-home";
         };
-        #headscale-passthrough = {
-         # entryPoints = [ "headscale" ];
-          #rule = "HostSNI(`*`)";
-          #service = "headscale-home";
-        #};
       };
       services = {
         home-https.loadBalancer.servers = [{ address = "10.100.0.2:443"; }];
         home-http.loadBalancer.servers  = [{ address = "10.100.0.2:80"; }];
         mc-home.loadBalancer.servers    = [{ address = "10.100.0.2:33333"; }];
-        # headscale-home.loadBalancer.servers = [{ address = "10.100.0.2:4090"; }];
       };
     };
   };
