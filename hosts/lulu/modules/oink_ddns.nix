232821114ef313f0e8a72ce021d38f1be29888e5
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-11 00:49:24 -0400

    removing secrets file

--- hosts/lulu/modules/oink_ddns.nix
+++ hosts/lulu/modules/oink_ddns.nix
@@ -22,13 +22,13 @@ in
         { domain = "eternaladventure.xyz"; subdomain = cfg.subdomain; ttl = 600; }
       ];
     };
-  };
+  
+    sops.secrets."porkbun/apikey" = {
+      sopsFile = ../../../secrets/porkbun_secrets.yaml;
+    };
 
-  sops.secrets."porkbun/apikey" = {
-    sopsFile = ../../../secrets/porkbun_secrets.yaml;
-  };
-
-  sops.secrets."porkbun/secretapikey" = {
-    sopsFile = ../../../secrets/porkbun_secrets.yaml;
+    sops.secrets."porkbun/secretapikey" = {
+      sopsFile = ../../../secrets/porkbun_secrets.yaml;
+    };
   };
 }
