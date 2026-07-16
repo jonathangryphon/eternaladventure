b63fd8249caf7e867ac9a7f4e984d6cd79e12b1f
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-16 09:34:18 -0400

    adding headscale auth key to client module and secrets

--- modules/headscale-client.nix
+++ modules/headscale-client.nix
@@ -1,6 +1,11 @@
 { config, ... }: {
+  sops.secrets."headscale/authkey" = {
+    sopsFile = ../secrets/headscale_auth_key.yaml;
+  };
+
   services.tailscale = {
     enable = true;
+    authKeyFile = config.sops.secrets."headscale/authkey".path;
     extraUpFlags = [
       "--login-server" "https://headscale.eternaladventure.xyz"
       "--accept-dns=true"
