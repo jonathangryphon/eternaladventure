89fafbc0491c2bfd28d428fdd915e3dd0da60f44
Jonathan Gryphon <jonathangryphon@protonmail.com>
2026-07-17 18:35:35 -0400

    reversing do to infinite recu.

--- modules/services.nix
+++ modules/services.nix
@@ -1,11 +1,11 @@
 {config, pkgs, lib, sops-nix, ... }:
 {
     imports = [
-        ../../modules/services/nextcloud.nix
-        ../../modules/services/jellyfin.nix
-        ../../modules/services/ente.nix
-        ../../modules/services/traefik-dashboard.nix
-        ../../modules/services/minecraft-server.nix
-        ../../modules/services/headscale-server.nix 
+        ./services/nextcloud.nix
+        ./services/jellyfin.nix
+        ./services/ente.nix
+        ./services/traefik-dashboard.nix
+        ./services/minecraft-server.nix
+        ./services/headscale-server.nix 
     ];
 }
