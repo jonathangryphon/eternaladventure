0b4a3030b3770edddc8995e6cac3db078510853d
Jonathan Gryphon <147462298+jonathangryphon@users.noreply.github.com>
2026-07-19 19:30:33 -0400

    updating dock part in darwin

--- hosts/gilbert/default.nix
+++ hosts/gilbert/default.nix
@@ -4,7 +4,6 @@
   imports = [ ./homebrew.nix ];
 
   nix.enable = false;
-  
   networking.hostName = "Gilbert";
 
   security.pam.services.sudo_local.touchIdAuth = true;
@@ -19,7 +18,31 @@
       ShowPathbar = true;
       ShowStatusBar = true;
     };
+    dock = {
+      persistent-apps = [
+        "Applications/Safari.app"
+        "Applications/Orion.app"
+        "Applications/DuckDuckGo.app"
+        "Applications/Firefox.app"
+        "Applications/Spotify.app"
+        "Applications/Signal.app"
+        "Applications/Whatsapp.app"
+        "Applications/Ente Auth.app"
+        "Applications/Terminal.app"
+        "Applications/Messages.app"
+        "Applications/VSCodium.app"
+        "Applications/Joplin.app"
+      ];
+      persistent-others = [ ];
+      autohide = true;
+      show-recents = false;
+    }
   };
+
+  # Safari default search engine + default browser (requires `duti` in systemPackages)
+  system.activationScripts.postActivation.text = ''
+    sudo -u jonathangryphon defaults write com.apple.Safari SearchProviderShortName -string "DuckDuckGo"
+  '';
 
   programs.zsh = {
     enable = true;
@@ -42,6 +65,7 @@
     sherlock
     exiftool
     asciiquarium
+    duti
   ];
 
   system.stateVersion = 6;
