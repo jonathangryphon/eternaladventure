606744e5de66acc65dbc1c2efd10bddb1474ea12
Jonathan Gryphon <147462298+jonathangryphon@users.noreply.github.com>
2026-07-19 20:00:04 -0400

    configuring further settings

--- hosts/gilbert/default.nix
+++ hosts/gilbert/default.nix
@@ -9,7 +9,7 @@
   security.pam.services.sudo_local.touchIdAuth = true;
 
   system.primaryUser = "charity";
-  system.defaults.screencapture.location = "~/testphotos/screenshots";
+  system.defaults.screencapture.location = "~/screenshots";
   
   system.defaults = {
     finder = {
@@ -28,23 +28,54 @@
         "Applications/Signal.app"
         "Applications/Whatsapp.app"
         "Applications/Ente Auth.app"
-        "Applications/Terminal.app"
-        "Applications/Messages.app"
+        "/System/Applications/Utilities/Terminal.app"
+        "/System/Applications/Messages.app"
         "Applications/VSCodium.app"
         "Applications/Joplin.app"
       ];
       persistent-others = [ ];
       autohide = true;
       show-recents = false;
+      wvous-tl-corner = 1; # 1 = disabled
+      wvous-tr-corner = 1;
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;      
     };
+    NSGlobalDomain = {
+      AppleLocale = "en_US@rg=gbzzzz"; # US language, GB (dd/mm/yy) region formatting
+      AppleMetricUnits = 1;
+      AppleMeasurementUnits = "Centimeters";
+      AppleICUForce24HourTime = true;
+    };
+    menuExtraClock = {
+      Show24Hour = true;
+      ShowDate = 1; # always show date
+      DateFormat = "EEE d MMM"; # e.g. "Sun 19 Jul"
+    };    
   };
 
   programs.zsh = {
     enable = true;
     shellInit = ''
       eval "$(/opt/homebrew/bin/brew shellenv)"
+      alias vim="nvim -u /etc/nvim-minimal.vim"
+      alias cat="bat"
     '';
   };
+
+  environment.etc."nvim-minimal.vim".text = ''
+    syntax on
+    set number
+    set tabstop=2
+    set shiftwidth=2
+    set expandtab
+  '';
+
+  environment.etc."nanorc".text = ''
+    include "${pkgs.nanorc}/share/nanorc/*.nanorc"
+    set linenumbers
+    set tabsize 2
+  '';
 
   environment.systemPackages = with pkgs; [
     git
@@ -61,6 +92,9 @@
     exiftool
     asciiquarium
     duti
+    bat
+    neovim
+    nanorc
   ];
 
   system.stateVersion = 6;
